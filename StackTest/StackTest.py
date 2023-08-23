fe00889cd6cdf8200c5e7447a234682c1b3a78e6
Russell Doughty <rustymonroe@gmail.com>
2023-08-23 16:46:29 -0500

    Add several llms to the run

--- StackTest/StackTest.py
+++ StackTest/StackTest.py
@@ -6,10 +6,16 @@ from langchain.llms import GPT4All
 from langchain import PromptTemplate, LLMChain
 
 # Path to weights
-PATH = 'G:/LLMs/GPT4All-13B-snoozy.ggmlv3.q4_0.bin'
+snoozyPath = 'G:/LLMs/GPT4All-13B-snoozy.ggmlv3.q4_0.bin'
+# gptPath    = 'G:/LLMs/chatgpt-gpt-3.5-turbo.txt'
+falconPath = 'G:/LLMs/ggml-model-gpt4all-falcon-q4_0.bin'
+wizardPath = 'G:/LLMs/GPT4All-13B-snoozy.ggmlv3.q4_0.bin'
 
 # Instance of llm
-myLlm = GPT4All(model = PATH, verbose = True)
+snoozyLlm = GPT4All(model = snoozyPath, verbose = True)
+# gptLlm    = GPT4All(model = gptPath, verbose = True)
+falconLlm = GPT4All(model = falconPath, verbose = True)
+wizardLlm = GPT4All(model = wizardPath, verbose = True)
 
 # Prompt template
 promptTemp = PromptTemplate(input_variables = ['question'],
@@ -20,7 +26,10 @@ promptTemp = PromptTemplate(input_variables = ['question'],
                         """)
 
 # LLM Chain
-chain = LLMChain(prompt = promptTemp, llm = myLlm)
+snoozyChain = LLMChain(prompt = promptTemp, llm = snoozyLlm)
+# gptChain    = LLMChain(prompt = promptTemp, llm = gptLlm)
+falconChain = LLMChain(prompt = promptTemp, llm = falconLlm)
+wizardChain = LLMChain(prompt = promptTemp, llm = wizardLlm)
 
 # Title
 st.title('GPT Stack Test for Y\'all')
@@ -30,8 +39,23 @@ myPrompt = st.text_input('Give me your prompt!')
 
 # if we hit enter
 if myPrompt:
-    # Pass prompt to chain
-    response = chain.run(myPrompt)
+    # Snoozy
+    snoozyResponse = snoozyChain.run(myPrompt)
+    st.write('Snoozy says:')
+    st.write(snoozyResponse)
     
-    # Return response
-    st.write(response)
+    # GPT
+    # gptResponse = gptChain.run(myPrompt)
+    # st.write('ChatGPT 3.5 Turbo says:')
+    # st.write(gptResponse)
+    
+    # Falcon
+    falconResponse = falconChain.run(myPrompt)
+    st.write('GPT4All Falcon says:')
+    st.write(falconResponse)
+    
+    # Wizard
+    wizardResponse = wizardChain.run(myPrompt)
+    st.write('Wizard says:')
+    st.write(wizardResponse)
+ 
